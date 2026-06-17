7a80bf0e8939924cae6e51dad65f64eed0ef1c55
ponponuzsvg <ponponuztelegrambro@gmail.com>
2026-06-17 15:30:13 +0200

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -3,13 +3,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/redirect', (req, res) => {
-  const { code } = req.query;
+  const { code, access_token, error } = req.query;
 
-  if (!code) {
-    return res.redirect(`teastrapp://oauth2callback?error=no_code`);
+  if (code) {
+    return res.redirect(`teastrapp://oauth2callback?access_token=test_${code}`);
   }
 
-  return res.redirect(`teastrapp://oauth2callback?access_token=test_${code}`);
+  if (error) {
+    return res.redirect(`teastrapp://oauth2callback?error=${error}`);
+  }
+
+  if (access_token) {
+    return res.redirect(`teastrapp://oauth2callback?access_token=${access_token}`);
+  }
+
+  res.status(400).send('Missing code or token');
 });
 
 app.get('/', (req, res) => {
