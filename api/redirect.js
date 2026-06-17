a11825087e8d973c8606d7c867718d6d4a7c6e5e
ponponuzsvg <ponponuztelegrambro@gmail.com>
2026-06-17 14:59:15 +0200

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,4 +1,8 @@
-module.exports = (req, res) => {
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/redirect', (req, res) => {
   const { code } = req.query;
 
   if (!code) {
@@ -6,4 +10,8 @@ module.exports = (req, res) => {
   }
 
   return res.redirect(`teastrapp://oauth2callback?access_token=test_${code}`);
-};
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
